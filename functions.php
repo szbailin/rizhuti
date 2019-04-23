24e8cf23119e9b55c9b60f29a19f7863d537ada6
AdFate <testritian@gmail.com>
2019-04-23 10:20:10 -0400

    修复下载500错误

--- functions.php
+++ functions.php
@@ -2,8 +2,30 @@
 // Require theme functions
 require get_stylesheet_directory() . '/inc/functions-theme.php';
 
+
+function rizhuti_unlock_url($txt, $key)
+{
+    $txt   = urldecode($txt);
+    $chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-=+";
+    $ch    = $txt[0];
+    $nh    = strpos($chars, $ch);
+    $mdKey = md5($key . $ch);
+    $mdKey = substr($mdKey, $nh % 8, $nh % 8 + 7);
+    $txt   = substr($txt, 1);
+    $tmp   = '';
+    $i     = 0;
+    $j     = 0;
+    $k     = 0;
+    for ($i = 0; $i < strlen($txt); $i++) {
+        $k = $k == strlen($mdKey) ? 0 : $k;
+        $j = strpos($chars, $txt[$i]) - $nh - ord($mdKey[$k++]);
+        while ($j < 0) {
+            $j += 64;
+        }
+        $tmp .= $chars[$j];
+    }
+    return base64_decode($tmp);
+}
+
 // Customize your functions
-// 本文件只有7行代码，如果多出来其他不明函数，请检测您的其他主题是否有木马感染！
-// 
-// 
-//
+ 
